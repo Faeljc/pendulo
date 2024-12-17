2ecc60d9f91eedd81c4d24b57c78a71a890f3278
faeljc <orafa_12@hotmail.com>
2024-12-17 17:24:22 -0300

    envio da porta COM para o server socket

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ const WebSocket = require('ws');
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
-// Configura a porta serial
-const port = new SerialPort({
-    path: 'COM9', // Substitua pela sua porta
-    baudRate: 9600, // Taxa de transmissão
-});
-
-// Configura o parser para ler linha a linha
-const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
-
-// Lida com erros de conexão
-port.on('error', (err) => {
-    console.error('Erro na porta serial:', err.message);
-});
 
 //Cria uma nova conexão e um servidor é inicializado na porta 3000
 const WebSocketServer = new WebSocket.Server({
@@ -25,6 +12,21 @@ const WebSocketServer = new WebSocket.Server({
 WebSocketServer.on('connection', (socket) => {
     socket.on("message", (msg) => {
         console.log('Recebido: ', msg.toString());
+
+        // Configura a porta serial
+        const port = new SerialPort({
+            path: msg.toString(), // Porta serial enviada do cliente
+            baudRate: 9600, // Taxa de transmissão
+        });
+
+        // Configura o parser para ler linha a linha
+        const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+        // Lida com erros de conexão
+        port.on('error', (err) => {
+            console.error('Erro na porta serial:', err.message);
+            socket.send(`Erro na porta serial:, ${err.message}`)
+        });
 
         // Evento para quando os dados são recebidos
         parser.on('data', (result) => {
